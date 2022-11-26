c1899320d0354b6d43392d354a2292e3a4a2fd9d
satohitoshi1 <satohitoshi121@gmail.com>
2022-11-26 10:38:58 +0900

    add app git hitoshi

--- Tes.py
+++ Tes.py
@@ -2,20 +2,26 @@ import sys
 from PIL import Image
 import pyocr
 import pyocr.builders
+import cv2
 
 # import MeCab
+
+kj = cv2.imread("kj_png/kj_01.png")
+kj_gray = cv2.cvtColor(kj, cv2.COLOR_BGR2GRAY)
+cv2.imwrite("kj_gray.png", kj_gray)
+# print(kj_gray)
 
 tools = pyocr.get_available_tools()
 if len(tools) == 0:
     print("No OCR tool found")
     sys.exit(1)
-# 推奨している順で読み込むので、配列の最初に推奨順の1つ目がはいる
+# 推奨している順で読み込むので、配列の最初に推奨順の1つ目がはいるread
 tool = tools[0]
 print("Will use tool '%s'" % (tool.get_name()))
-# 例: Will use tool 'Tesseract (sh)'
+
 
 txt = tool.image_to_string(
-    Image.open("kj_png/kj_01.png"),  # OCRする画像
+    Image.open("kj_gray.png"),  # OCRする画像
     lang="jpn",  # 学習済み言語データ
     builder=pyocr.builders.TextBuilder(tesseract_layout=4),  # 期待される出力のタイプを指定
 )
